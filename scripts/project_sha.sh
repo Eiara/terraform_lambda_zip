997ad9c50f755fdd8c6593b322cef5dd5fc4ec07
aurynn shaw <self@aurynn.com>
2019-05-29 10:07:37 +1200

    Add additional logging output

--- scripts/project_sha.sh
+++ scripts/project_sha.sh
@@ -4,13 +4,15 @@ set -e
 eval "$(jq -r '@sh "PYTHON_PROJECT=\(.project_path)"')"
 
 if ! [ -d $PYTHON_PROJECT ]; then
-  >&2 echo "python project does not exist!"
+  >&2 echo "ERROR: python project does not exist!"
   exit 1
 fi
 
 if (find -s > /dev/null 2>&1); then
+  echo "DEBUG: Using find -s to identify python project"
   PROJECT_HASH=$(find -s $PYTHON_PROJECT -type f -not -iname requirements.txt | cpio -o --quiet | shasum -a 256 | cut -d " " -f 1)
 else
+  echo "DEBUG: Using find | sort to identify python project"
   PROJECT_HASH=$(find $PYTHON_PROJECT -type f -not -iname requirements.txt | sort | cpio -o --quiet | shasum -a 256 | cut -d " " -f 1)
 fi
 
