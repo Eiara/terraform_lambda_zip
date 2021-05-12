e81b3cd151c2f431f9c1411b0d868a8e0757030c
Aurynn Shaw <self@aurynn.com>
2021-05-12 12:09:32 +1200

    0.13 upgrade

--- versions.tf
+++ versions.tf
@@ -1,4 +1,12 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    external = {
+      source = "hashicorp/external"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
 }
